a618091285f0ad4d62db024c7a529aac910be63f
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2025-07-01 02:21:21 +0000

    utils/detect_unauthorized_packages.py: fix f strings & clarify header doco re: renaming dirs

--- utils/detect_unauthorized_packages.py
+++ utils/detect_unauthorized_packages.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 # Check for .spack dir's in installation space and confirm against
-# authoritative NCO approved software list.
+# authoritative NCO approved software list. Renames installation directories
+# for non-approved packages to prevent accidental build caching/transfer to
+# WCOSS2.
 
 # This utility can be run after every "spack install", as well as on a cron
 # (especially for monitoring automated builds).
@@ -22,7 +24,7 @@ with open(approved_list_path, "r") as approved_file:
 dotspack_dirs = [d for d in glob.glob(installation_root + "/**/.*", recursive=True) if os.path.basename(d)==".spack" and os.path.isdir(d)]
 
 success = True
-email_body = f"ALERT: One or more non-NCO authorized packages were found under {installation_root} on Acorn, using the {approved_list_path) approved package list:\n"
+email_body = f"ALERT: One or more non-NCO authorized packages were found under {installation_root} on Acorn, using the {approved_list_path} approved package list:\n"
 
 for dotspack_dir in dotspack_dirs:
     with open(os.path.join(dotspack_dir, "spec.json"), "r") as spec_file:
@@ -31,7 +33,7 @@ for dotspack_dir in dotspack_dirs:
     if pkg_name not in approved_list:
         success = False
         pkg_dir = os.path.dirname(dotspack_dir)
-        alert = (f"Package {pkg_name} installed at {pkg_dir} on Acorn not approved by NCO!")
+        alert = (f"Package '{pkg_name}' installed at {pkg_dir} on Acorn not approved by NCO!")
         print("ALERT:", alert)
         renamed_pkg_dir = pkg_dir + ".disabled"
         print(f"    Renaming {pkg_dir} to\n        {renamed_pkg_dir} to prevent build caching.")
